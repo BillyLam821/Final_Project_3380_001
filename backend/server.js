e6aa233c05b15880e741c280d4c2137cb3fc4aeb
BillyLam821 <BillyLam821@gmail.com>
2023-04-08 15:55:25 -0700

    added prod server lines

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
@@ -10,9 +11,22 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 app.use("/api/facility", require("./routes/facilityRoutes"));
 app.use("/api/comments", require("./routes/commentRouter"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../finalproject3600/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "finalproject3600", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
